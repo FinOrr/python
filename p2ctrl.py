365e9c26186b29a4a78867f99f4588b945048e52
Fin <finorr3@gmail.com>
2023-04-13 22:52:37 +0700

    Update p2ctrl.py

--- p2ctrl.py
+++ p2ctrl.py
@@ -6,13 +6,6 @@ import torch.optim as optim
 import random
 import matplotlib.pyplot as plt
 
-import serial
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import random
-
 # Define hyperparameters
 BATCH_SIZE = 32
 GAMMA = 0.99
@@ -25,84 +18,14 @@ MAX_TIMESTEPS = 1000
 HIDDEN_SIZE = 128
 LR = 0.001
 
-# Define the MLP model
-class MLP(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size):
-        super(MLP, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, output_size)
+#DQN: This class defines the architecture of the DQN with MLP and implements the training process.
 
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+#ReplayMemory: This class is responsible for storing and retrieving experiences, which are tuples containing the state, action, reward, next state, and done flag.
 
-# Define the DQN class
-class DQN(object):
-    def __init__(self, input_size, output_size, hidden_size):
-        self.policy_net = MLP(input_size, output_size, hidden_size)
-        self.target_net = MLP(input_size, output_size, hidden_size)
-        self.target_net.load_state_dict(self.policy_net.state_dict())
-        self.target_net.eval()
-        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=LR)
-        self.memory = ReplayMemory(10000)
+#Agent: This class acts as the interface between the DQN and the environment. It receives observations from the environment, selects actions using the DQN, and updates the DQN using experiences from the replay memory.
 
-    def select_action(self, state, eps):
-        if random.random() < eps:
-            return torch.tensor([[random.randrange(output_size)]], dtype=torch.long)
-        else:
-            with torch.no_grad():
-                return self.policy_net(state).max(1)[1].view(1, 1)
+#Environment: This class simulates the gas chamber system and provides the state and reward information to the agent.
 
-    def optimize_model(self):
-        if len(self.memory) < BATCH_SIZE:
-            return
-        transitions = self.memory.sample(BATCH_SIZE)
-        batch = Transition(*zip(*transitions))
+#Pod: This class encapsulates the details of the chamber system, such as the equations that govern its behavior.
 
-        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                          batch.next_state)), device=device, dtype=torch.bool)
-        non_final_next_states = torch.cat([s for s in batch.next_state
-                                                    if s is not None])
-        state_batch = torch.cat(batch.state)
-        action_batch = torch.cat(batch.action)
-        reward_batch = torch.cat(batch.reward)
-
-        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
-
-        next_state_values = torch.zeros(BATCH_SIZE, device=device)
-        next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
-        expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-
-        loss = nn.functional.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
-
-        self.optimizer.zero_grad()
-        loss.backward()
-        for param in self.policy_net.parameters():
-            param.grad.data.clamp_(-1, 1)
-        self.optimizer.step()
-
-    def update_target_net(self):
-        self.target_net.load_state_dict(self.policy_net.state_dict())
-
-class ReplayMemory:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
-
-    def push(self, state, action, next_state, reward):
-        """Push a transition to the memory."""
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = (state, action, next_state, reward)
-        self.position = (self.position + 1) % self.capacity
-
-    def sample(self, batch_size):
-        """Sample a batch of transitions from the memory."""
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
+# Plotter: This class provides a visual representation of the training process, displaying the training progress and performance metrics over time.
